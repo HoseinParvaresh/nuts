fb04ac4fca23a233a744c9d7ff7e115ab8b96eee
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-10-23 00:13:59 +0330

    fix header

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -8,17 +8,18 @@ import { HiMiniBars3 } from "react-icons/hi2";
 export default function Header() {
 	return (
 		<div className="relative border-b border-primary/20">
-			<img src="/images/top-header.png" alt="top-header" />
+			<img className="hidden md:block" src="/images/top-header.png" alt="top-header" />
+			<img className="block md:hidden" src="/images/top-header-mobile.png" alt="top-header" />
 			<div className="pb-2 md:pb-4 pt-2 md:pt-5">
-				<div className="flex gap-8 xl:gap-0 items-center md:items-start lg:items-center justify-between px-5 md:px-10 xl:px-27.5">
+				<div className="flex gap-5 2xs:gap-8 xl:gap-0 items-center md:items-start lg:items-center justify-between px-5 md:px-10 xl:px-27.5">
 					{/* navbar */}
 					<div className="md:hidden">
 						<HiMiniBars3 />
 					</div>
 					{/* logo */}
-					<div className="flex-center gap-3">
-						<img className="w-12 xl:w-15" src="/images/logo1.png" alt="logo" />
-						<p className="text-primary font-bold text-xl xl:text-3xl min-w-max">Nuts & Spices</p>
+					<div className="flex-center gap-1 xs:gap-3">
+						<img className="w-17 2xs:w-8 xs:w-12 xl:w-15 hidden 2xs:block" src="/images/logo1.png" alt="logo" />
+						<p className="text-primary font-bold text-base/5 2xs:text-sm xs:text-xl xl:text-3xl w-[53px] 2xs:min-w-max">Nuts & Spices</p>
 					</div>
 					{/* title and search box and number */}
 					<div className="flex flex-col items-center justify-center gap-5">
@@ -34,20 +35,20 @@ export default function Header() {
 						{/* number */}
 						<div className="flex justify-end md:justify-center items-center flex-wrap gap-2 md:gap-5">
 							<a href="" className="flex-center gap-1">
-								<FaWhatsapp className="size-5 md:size-5.5 text-green-500" />
-								<span className="text-[13px] md:text-sm  text-primary">+989931882813</span>
+								<FaWhatsapp className="size-4 xs:size-5 md:size-5.5 text-green-500" />
+								<span className="text-xs md:text-sm  text-primary">+989931882813</span>
 							</a>
 							<a href="" className="flex-center gap-1">
-								<FaTelegram className="size-5 md:size-5.5 text-sky-500" />
-								<span className="text-[13px] md:text-sm  text-primary">+989931882813</span>
+								<FaTelegram className="size-4 xs:size-5 md:size-5.5 text-sky-500" />
+								<span className="text-xs md:text-sm  text-primary">+989931882813</span>
 							</a>
 							<a href="" className="flex-center gap-1">
-								<FaInstagram className="size-5 md:size-5.5 text-pink-500" />
-								<span className="text-[13px] md:text-sm  text-primary">+989931882813</span>
+								<FaInstagram className="size-4 xs:size-5 md:size-5.5 text-pink-500" />
+								<span className="text-xs md:text-sm  text-primary">+989931882813</span>
 							</a>
 							<a href="" className="flex-center gap-1">
-								<FaWhatsapp className="size-5 md:size-5.5 text-green-500" />
-								<span className="text-[13px] md:text-sm  text-primary">+989931882813</span>
+								<FaWhatsapp className="size-4 xs:size-5 md:size-5.5 text-green-500" />
+								<span className="text-xs md:text-sm  text-primary">+989931882813</span>
 							</a>
 						</div>
 					</div>
