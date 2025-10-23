a886bc3db9edc2108169a0f075a6b7ceb2b0548b
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-10-23 21:29:10 +0330

    complete heaedr and navbar

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -3,8 +3,8 @@
 import { FiSearch } from "react-icons/fi";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
 import { HiMiniBars3 } from "react-icons/hi2";
+import { HiMiniXMark } from "react-icons/hi2";
 import { useState } from "react";
 export default function Header() {
 	const [toggle, setToggle] = useState<string>("hidden");
@@ -19,19 +19,19 @@ export default function Header() {
 			{/* layout */}
 			<div onClick={navbarToggle} className={`w-full ${toggle} fixed h-screen z-10 bg-black/30`}></div>
 			{/* header */}
-			<div className="flex justify-between px-12 py-5">
+			<div className="flex justify-between items-start px-2 4xs:px-3 2xs:px-5 lg:px-12 py-5 gap-3 4xs:gap-4 2xs:gap-5">
 				{/* bar icon */}
-				<div onClick={navbarToggle} className="md:hidden">
-					<HiMiniBars3 className="size-5" />
+				<div onClick={navbarToggle} className="md:hidden mt-1">
+					<HiMiniBars3 className="size-7" />
 				</div>
 				{/* group name */}
-				<img className="size-60" src="/images/logo2.png" alt="logo" />
+				<img className="size-40 lg:size-50 xl:size-60 hidden md:block" src="/images/logo2.png" alt="logo" />
 				{/* title and search box and number */}
-				<div className="flex flex-col item-center gap-5">
-					{/* top header image */}
-					<p className="text-5xl font-interBold text-center mb-1">Iran Nuts And Spices Trading Group</p>
+				<div className="flex flex-col item-center gap-5 w-full md:w-auto">
+					{/* top header text */}
+					<p className="text-[27px]/9 xs:text-4xl md:text-3xl lg:text-4xl xl:text-5xl font-interBold text-left mb-1">Iran Nuts And Spices Trading Group</p>
 					{/* search box */}
-					<div className="flex-center w-full hidden md:flex">
+					<div className="flex-center w-full flex">
 						<div className="border-y-2 border-l-2 border-secondary rounded-l-md w-full">
 							<input placeholder="Search products..." className="outline-none w-full pl-2 p-1 md:p-2 placeholder:text-sm md:placeholder:text-base placeholder:text-primary text-primary" type="text" />
 						</div>
@@ -40,56 +40,42 @@ export default function Header() {
 						</div>
 					</div>
 					{/* number */}
-					<div className="flex flex-col gap-4">
+					<div className="flex flex-col gap-4 ">
 						<a href="" className="flex-center gap-2">
-							<FaWhatsapp className="size-10 text-green-500" />
-							<span className="text-orange-400 font-interBold text-3xl">WhatsApp ( Country IRAN ) +989931882813</span>
+							<FaWhatsapp className="size-8 lg:size-10 text-green-500" />
+							<span className="text-orange-400 font-interBold text-lg 2xs:text-xl lg:text-2xl xl:text-3xl">WhatsApp ( Country IRAN ) +989931882813</span>
 						</a>
 						<a href="" className="flex-center gap-2">
-							<FaTelegram className="size-10 text-sky-500" />
-							<span className="text-orange-400 font-interBold text-3xl">Telegram ( Country IRAN ) +989931882813</span>
+							<FaTelegram className="size-8 lg:size-10 text-sky-500" />
+							<span className="text-orange-400 font-interBold text-lg 2xs:text-xl lg:text-2xl xl:text-3xl">Telegram ( Country IRAN ) +989931882813</span>
 						</a>
 					</div>
 				</div>
 				{/* logo */}
-				<img className="h-60 w-70" src="/images/logo1.png" alt="logo" />
+				<img className="h-40 lg:h-50 xl:h-60 w-50 lg:w-60 xl:w-70 hidden md:block" src="/images/logo1.png" alt="logo" />
 			</div>
 			{/* navbar */}
-			<div className={`fixed px-3 bg-white h-screen top-0 ${navToggle} w-65 z-20 transition-all duration-300`}>
-				{/* logo */}
-				<div className="flex-center gap-3 flex mt-5">
-					<img className="w-8 xl:w-10" src="/images/logo2.png" alt="logo" />
-					<p className="text-primary font-bold text-xl xl:text-3xl min-w-max">I.N.S.T Group</p>
+			<div className={`fixed flex flex-col px-3 bg-white h-screen top-0 ${navToggle} w-67 z-20 transition-all duration-300`}>
+				{/* marks icon */}
+				<div className="self-end" onClick={navbarToggle}>
+					<HiMiniXMark className="size-8 my-3" />
 				</div>
-				{/* search */}
-				<div className="flex-center w-full flex mt-5">
-					<div className="border-y-2 border-l-2 border-secondary rounded-l-md w-full">
-						<input placeholder="Search products..." className="outline-none w-full pl-2 p-1 md:p-2 placeholder:text-sm md:placeholder:text-base placeholder:text-primary text-primary" type="text" />
-					</div>
-					<div className="bg-secondary text-primary p-[5.6px] md:p-2.5 border border-secondary rounded-r-md cursor-pointer">
-						<FiSearch className="size-5.5" />
-					</div>
+				{/* logo */}
+				<div className="flex">
+					<img className="size-30" src="/images/logo2.png" alt="logo" />
+					<img className="h-30 w-31" src="/images/logo1.png" alt="logo" />
 				</div>
 				{/* contact */}
-				<div className="mt-10">
-					<p className="font-bold text-lg">Contact us</p>
-					{/* number */}
-					<div className="flex flex-col items-start gap-y-3 mt-4">
-						<a href="" className="flex-center gap-1">
-							<FaWhatsapp className="size-5.5 text-green-500" />
-							<span className="text-primary">+989931882813</span>
+				<div className="flex flex-col gap-4">
+					<p className="font-interBold mt-7 text-lg">Contact us (Country IRAN)</p>
+					<div>
+						<a href="" className="flex-center gap-3 mb-5">
+							<FaWhatsapp className="size-10 text-green-500" />
+							<span className="text-orange-400 font-interBold text-xl">WhatsApp +989931882813</span>
 						</a>
-						<a href="" className="flex-center gap-1">
-							<FaTelegram className="size-5.5 text-sky-500" />
-							<span className="text-primary">+989931882813</span>
-						</a>
-						<a href="" className="flex-center gap-1">
-							<FaInstagram className="size-5.5 text-pink-500" />
-							<span className="text-primary">+989931882813</span>
-						</a>
-						<a href="" className="flex-center gap-1">
-							<FaWhatsapp className="size-5.5 text-green-500" />
-							<span className="text-primary">+989931882813</span>
+						<a href="" className="flex-center gap-3">
+							<FaTelegram className="size-10 text-sky-500" />
+							<span className="text-orange-400 font-interBold text-xl">Telegram +989931882813</span>
 						</a>
 					</div>
 				</div>
